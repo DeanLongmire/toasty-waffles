957ea2f4ff9fc7b57ddf3eb7d2e94df238061c20
Dean Longmire <deanlongmire100@gmail.com>
2024-04-13 14:26:37 -0400

    Format code

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,8 +2,8 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class ApplicationRoute extends Route {
-    @service router;
-    beforeModel() {
-        this.router.transitionTo('home');
-    }
+  @service router;
+  beforeModel() {
+    this.router.transitionTo('home');
+  }
 }
