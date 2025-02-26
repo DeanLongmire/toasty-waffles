48055d9f7b10ae5c3c01a7e9ff9573f66b9c116b
Dean Longmire <deanlongmire100@gmail.com>
2025-02-25 19:37:48 -0500

    Make linting changes

--- app/controllers/seasons/season.js
+++ app/controllers/seasons/season.js
@@ -1,7 +1,3 @@
 import Controller from '@ember/controller';
-import { service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 
-export default class SeasonsSeasonController extends Controller {
-    
-}
+export default class SeasonsSeasonController extends Controller {}
