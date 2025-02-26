40f4190ee40c015f51a892503d5bf6ca3bc295f5
Dean Longmire <deanlongmire100@gmail.com>
2025-02-25 19:51:30 -0500

    Fix mirage with string ids

--- mirage/config.js
+++ mirage/config.js
@@ -101,6 +101,7 @@ function routes() {
               id: division.id,
               divisionName: division.divisionName,
               teams: division.teams.map((team) => {
+                console.log(team.id)
                 return {
                   id: team.id,
                   teamName: team.teamName,
--- mirage/fixtures/year.js
+++ mirage/fixtures/year.js
@@ -1,4 +1,4 @@
 export default [
-  { id: 1, startYear: '2023', endYear: '2024', champion: '10', loser: '1' },
-  { id: 2, startYear: '2024', endYear: '2025', champion: '12', loser: '2' },
+  { id: 1, startYear: '2023', endYear: '2024', champion: '866543990391074816', loser: '998437145096478720' },
+  { id: 2, startYear: '2024', endYear: '2025', champion: '1001334659659190272', loser: '712424460816318464' },
 ];
