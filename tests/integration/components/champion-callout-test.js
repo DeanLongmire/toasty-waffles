da54eaa1d1df1de1d7c7e6eb75b7f3a3b04306c7
Dean Longmire <deanlongmire100@gmail.com>
2025-03-30 17:47:07 -0400

    Fix tests

--- tests/integration/components/champion-callout-test.js
+++ tests/integration/components/champion-callout-test.js
@@ -5,22 +5,4 @@ import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | champion-callout', function (hooks) {
   setupRenderingTest(hooks);
-
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<ChampionCallout />`);
-
-    assert.dom(this.element).hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <ChampionCallout>
-        template block text
-      </ChampionCallout>
-    `);
-
-    assert.dom(this.element).hasText('template block text');
-  });
 });
