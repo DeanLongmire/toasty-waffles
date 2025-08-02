b1678cbcd10fe611045b6922381d16d6f990426b
Dean Longmire <107720531+DeanLongmire@users.noreply.github.com>
2025-08-02 16:18:09 -0400

    Install dlg-ui (#6)

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,6 +4,35 @@ import { service } from '@ember/service';
 export default class ApplicationController extends Controller {
   @service store;
 
+  get navbarOptions() {
+    let years = [];
+    let index = 2;
+    this.model.years.forEach((year) => {
+      years.push({
+        label: year.yearRange,
+        route: 'seasons.season',
+        type: 'link',
+        index: index++,
+        param: year.yearRange,
+      });
+    });
+    return [
+      {
+        label: 'Teams',
+        route: 'teams',
+        type: 'link',
+        index: 0,
+      },
+      {
+        label: 'League',
+        route: 'league',
+        type: 'link',
+        index: 1,
+      },
+      ...years,
+    ];
+  }
+
   get league() {
     return this.model.league;
   }
