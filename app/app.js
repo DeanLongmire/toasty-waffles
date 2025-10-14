e1680520a1680c6bc2d1df6feffcaad748fb8c0b
Dean Longmire <deanlongmire100@gmail.com>
2025-10-14 18:49:00 -0400

    Add support for ember inspector

--- app/app.js
+++ app/app.js
@@ -1,14 +1,18 @@
 import Application from '@ember/application';
+import compatModules from '@embroider/virtual/compat-modules';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-import compatModules from '@embroider/virtual/compat-modules';
+import setupInspector from '@embroider/legacy-inspector-support/ember-source-4.12';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver.withModules(compatModules);
+  
+  // setup inspector for this application
+  inspector = setupInspector(this);
 }
 
 loadInitializers(App, config.modulePrefix, compatModules);
